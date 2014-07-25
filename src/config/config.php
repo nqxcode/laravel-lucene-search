d00085defb6586e88dae64c7fdb4ad9d8afa4ec3
nqxcode <nc101ux@gmail.com>
2014-07-26 03:32:16 +0400

    Add description to config.php

--- src/config/config.php
+++ src/config/config.php
@@ -6,20 +6,17 @@ return array(
      | Path to Lucene search index.
      |--------------------------------------------------------------------------
      |
-     | TODO Write description!
-     |
-     |
      */
     'index_path' => storage_path() . '/laravel-search/index',
 
     /*
      |--------------------------------------------------------------------------
      | List of models descriptions.
+     |--------------------------------------------------------------------------
      |
      | Each description should contain class of model and fields available
      | for search indexing.
      |
-     |--------------------------------------------------------------------------
      |
      | For example, one of model's description can be such as this:
      |
@@ -32,8 +29,23 @@ return array(
      */
     'models' => [],
 
-
+    /*
+     |----------     |----------------------------------------------------------------
+     | List of filter for search index analyzer.
+     |--------------------------------------------------------------------------
+     |
+     | Each filter is a class implements interface TokenFilterInterface
+     | from ZendSearch library.
+     |
+     |
+    */
     'filters' => [],
 
+    /*
+     |--------------------------------------------------------------------------
+     | List of files with stop words.
+     |--------------------------------------------------------------------------
+     |
+    */
     'stop_words_paths' => [],
 );
