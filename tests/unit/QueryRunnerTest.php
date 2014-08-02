33d7b00cd321903f793b2b18a68c1c170e054128
nqxcode <nc101ux@gmail.com>
2014-08-02 17:39:07 +0400

    Changes in tests for QueryRunner.

--- tests/unit/QueryRunnerTest.php
+++ tests/unit/QueryRunnerTest.php
@@ -32,7 +32,9 @@ class QueryRunnerTest extends TestCase
     {
         $this->queryRunner->rawQuery($query = 'test query');
         $this->queryRunner->rawQuery($query = new Boolean());
-        $this->queryRunner->rawQuery(function(){ return 'test query';});
+        $this->queryRunner->rawQuery(function () {
+            return 'test query';
+        });
         $this->setExpectedException('\InvalidArgumentException');
         $this->queryRunner->rawQuery(123);
     }
@@ -71,21 +73,21 @@ class QueryRunnerTest extends TestCase
     public function getWhereWithOptionsDataProvider()
     {
         $data = [
-            [['test:("test value")', 0] , ['test', 'test value', ['required' => false] ]],
-            [['test:("test value")', 0] , ['test', 'test value', ['prohibited' => true] ]],
-            [['test:("test value")', null] , ['test', 'test value', ['required' => false, 'prohibited' => false] ]],
-            [['test:(test value)', 1] , ['test', 'test value', ['phrase' => false] ] ],
-            [['test:("test~0.1 value~0.1")', 1] , ['test', 'test value', ['fuzzy' => 0.1] ]],
-            [['test:("test~ value~")', 1] , ['test', 'test value', ['fuzzy' => true] ]],
-            [['test:("test value")', 1] , ['test', 'test value', ['fuzzy' => false] ]],
-            [['test:(test~0.1 value~0.1)', 1] , ['test', 'test value', ['fuzzy' => 0.1, 'phrase' => false] ]],
-            [['test:("test value"~10)', 1] , ['test', 'test value', ['proximity' => 10] ]],
+            [['test:("test value")', 0], ['test', 'test value', ['required' => false]]],
+            [['test:("test value")', 0], ['test', 'test value', ['prohibited' => true]]],
+            [['test:("test value")', null], ['test', 'test value', ['required' => false, 'prohibited' => false]]],
+            [['test:(test value)', 1], ['test', 'test value', ['phrase' => false]]],
+            [['test:("test~0.1 value~0.1")', 1], ['test', 'test value', ['fuzzy' => 0.1]]],
+            [['test:("test~ value~")', 1], ['test', 'test value', ['fuzzy' => true]]],
+            [['test:("test value")', 1], ['test', 'test value', ['fuzzy' => false]]],
+            [['test:(test~0.1 value~0.1)', 1], ['test', 'test value', ['fuzzy' => 0.1, 'phrase' => false]]],
+            [['test:("test value"~10)', 1], ['test', 'test value', ['proximity' => 10]]],
 
-            [['field1:("value"~10) OR field2:("value"~10)', 1], [['field1', 'field2'], 'value', ['proximity' => 10] ]],
-            [['field1:("value~0.1"~10) OR field2:("value~0.1"~10)', 1], [['field1', 'field2'], 'value', ['proximity' => 10, 'fuzzy' => 0.1] ]],
+            [['field1:("value"~10) OR field2:("value"~10)', 1], [['field1', 'field2'], 'value', ['proximity' => 10]]],
+            [['field1:("value~0.1"~10) OR field2:("value~0.1"~10)', 1], [['field1', 'field2'], 'value', ['proximity' => 10, 'fuzzy' => 0.1]]],
         ];
 
-        $data =  array_merge($data, $this->getExpectedAndSourceDataForQueryWithSpecialOperators());
+        $data = array_merge($data, $this->getExpectedAndSourceDataForQueryWithSpecialOperators());
         $data = array_merge($data, $this->getExpectedAndSourceDataForQueryWithSpecialChars());
 
         return $data;
@@ -136,22 +138,22 @@ class QueryRunnerTest extends TestCase
     public function getFindWithOptionsDataProvider()
     {
         $data = [
-            [['test:(test value)', 0] , ['test', 'test value', ['required' => false] ]],
-            [['test:(test value)', 0] , ['test', 'test value', ['prohibited' => true] ]],
-            [['test:(test value)', null] , ['test', 'test value', ['required' => false, 'prohibited' => false] ]],
-            [['test:(test value)', 1] , ['test', 'test value', ['phrase' => false] ] ],
-            [['test:("test value")', 1] , ['test', 'test value', ['phrase' => true] ] ],
-            [['test:(test~0.1 value~0.1)', 1] , ['test', 'test value', ['fuzzy' => 0.1] ]],
-            [['test:(test~ value~)', 1] , ['test', 'test value', ['fuzzy' => true] ]],
-            [['test:(test value)', 1] , ['test', 'test value', ['fuzzy' => false] ]],
-            [['test:(test~0.1 value~0.1)', 1] , ['test', 'test value', ['fuzzy' => 0.1, 'phrase' => false] ]],
-            [['test:("test value"~10)', 1] , ['test', 'test value', ['proximity' => 10] ]],
+            [['test:(test value)', 0], ['test', 'test value', ['required' => false]]],
+            [['test:(test value)', 0], ['test', 'test value', ['prohibited' => true]]],
+            [['test:(test value)', null], ['test', 'test value', ['required' => false, 'prohibited' => false]]],
+            [['test:(test value)', 1], ['test', 'test value', ['phrase' => false]]],
+            [['test:("test value")', 1], ['test', 'test value', ['phrase' => true]]],
+            [['test:(test~0.1 value~0.1)', 1], ['test', 'test value', ['fuzzy' => 0.1]]],
+            [['test:(test~ value~)', 1], ['test', 'test value', ['fuzzy' => true]]],
+            [['test:(test value)', 1], ['test', 'test value', ['fuzzy' => false]]],
+            [['test:(test~0.1 value~0.1)', 1], ['test', 'test value', ['fuzzy' => 0.1, 'phrase' => false]]],
+            [['test:("test value"~10)', 1], ['test', 'test value', ['proximity' => 10]]],
 
-            [['field1:("value"~10) OR field2:("value"~10)', 1], [['field1', 'field2'], 'value', ['proximity' => 10] ]],
-            [['field1:("value~0.1"~10) OR field2:("value~0.1"~10)', 1], [['field1', 'field2'], 'value', ['proximity' => 10, 'fuzzy' => 0.1] ]],
+            [['field1:("value"~10) OR field2:("value"~10)', 1], [['field1', 'field2'], 'value', ['proximity' => 10]]],
+            [['field1:("value~0.1"~10) OR field2:("value~0.1"~10)', 1], [['field1', 'field2'], 'value', ['proximity' => 10, 'fuzzy' => 0.1]]],
         ];
 
-        $data =  array_merge($data, $this->getExpectedAndSourceDataForQueryWithSpecialOperators());
+        $data = array_merge($data, $this->getExpectedAndSourceDataForQueryWithSpecialOperators());
         $data = array_merge($data, $this->getExpectedAndSourceDataForQueryWithSpecialChars());
 
         return $data;
@@ -162,39 +164,112 @@ class QueryRunnerTest extends TestCase
         $this->queryRunner->rawQuery('test query');
         $this->queryRunner->limit(2, 3);
 
-        $this->queryRunner->addFilter(function($query){ return $query . ' first modification';});
-        $this->queryRunner->addFilter(function($query){ return $query . ' second modification';});
+        $this->queryRunner->addFilter(function ($query) {
+            return $query . ' first modification';
+        });
+        $this->queryRunner->addFilter(function ($query) {
+            return $query . ' second modification';
+        });
 
-        $query = 'test query first modification second modification';
+        $expectedQuery = 'test query first modification second modification';
 
-        $this->getAssertions($query);
+        $this->assertionsForTestingOfGettingResults($expectedQuery);
     }
 
-    private function getAssertions($query)
+    public function testGetForCallableRawQuery()
+    {
+        $this->queryRunner->rawQuery(function () {
+            return 'test query';
+        });
+        $this->queryRunner->limit(2, 3);
+
+        $this->queryRunner->addFilter(function ($query) {
+            return $query . ' first modification';
+        });
+        $this->queryRunner->addFilter(function ($query) {
+            return $query . ' second modification';
+        });
+
+        $expectedQuery = 'test query first modification second modification';
+
+        $this->assertionsForTestingOfGettingResults($expectedQuery);
+    }
+
+    public function testGetForZendSearchQueryRawQuery()
+    {
+        $query = new Boolean;
+        $query->addSubquery(QueryParser::parse('test query'));
+
+        $this->queryRunner->rawQuery($query);
+        $this->queryRunner->limit(2, 3);
+
+        $this->queryRunner->addFilter(function ($query) {
+            return $query->addSubquery(QueryParser::parse('first'));
+        });
+        $this->queryRunner->addFilter(function ($query) {
+            return $query->addSubquery(QueryParser::parse('second'));
+        });
+
+        $expectedQuery = new Boolean;
+        $expectedQuery->addSubquery(QueryParser::parse('test query'));
+        $expectedQuery->addSubquery(QueryParser::parse('first'));
+        $expectedQuery->addSubquery(QueryParser::parse('second'));
+
+        $this->assertionsForTestingOfGettingResults($expectedQuery);
+    }
+
+    public function testGetForQueryBuiltByConstructor()
+    {
+//        $this->query->shouldReceive('addSubquery')->with(m::on(function ($arg) {
+//            $query = QueryParser::parse('test query');
+//            $this->assertEquals($query, $arg);
+//            return true;
+//        }), true)->once()->byDefault();
+//
+//        $this->queryRunner->find('test query');
+//        $this->queryRunner->limit(2, 3);
+
+//        $this->query->shouldReceive('addSubquery')->with('first')->once();
+//        $this->query->shouldReceive('addSubquery')->with('second')->once();
+
+//        $this->queryRunner->addFilter(function ($query) {
+//            return $query->addSubquery(m::any());
+//        });
+//        $this->queryRunner->addFilter(function ($query) {
+//            return $query->addSubquery(m::any());
+//        });
+
+//        $this->assertionsForTestingOfGettingResults($this->query);
+    }
+
+    private function assertionsForTestingOfGettingResults($expectedQuery)
     {
         $this->search->shouldReceive('index->find')
-            ->with($query)
+            ->with(m::on(function ($arg) use ($expectedQuery) {
+                $this->assertEquals($expectedQuery, $arg);
+                return true;
+            }))
             ->andReturn($hits = [1, 2, 3, 4, 5])->once();
         $this->search->shouldReceive('config->models')->with([4, 5])->andReturn('test result')->once();
 
         $this->assertEquals('test result', $this->queryRunner->get());
         $this->assertEquals(5, $this->queryRunner->count());
-        $this->assertEquals($query, $this->queryRunner->getLastQuery());
+        $this->assertEquals($expectedQuery, $this->queryRunner->getLastQuery());
     }
 
     private function getExpectedAndSourceDataForQueryWithSpecialOperators()
     {
         return [
 
-            [['test:("test not value")', 1], ['test', 'test not value', ['phrase' => true] ]],
-            [['test:("test to value")', 1], ['test', 'test to value', ['phrase' => true] ]],
-            [['test:("test and value")', 1], ['test', 'test and value', ['phrase' => true] ]],
-            [['test:("test or value")', 1], ['test', 'test or value', ['phrase' => true] ]],
+            [['test:("test not value")', 1], ['test', 'test not value', ['phrase' => true]]],
+            [['test:("test to value")', 1], ['test', 'test to value', ['phrase' => true]]],
+            [['test:("test and value")', 1], ['test', 'test and value', ['phrase' => true]]],
+            [['test:("test or value")', 1], ['test', 'test or value', ['phrase' => true]]],
 
-            [['test:(test value)', 1], ['test', 'test not value', ['phrase' => false] ]],
-            [['test:(test value)', 1], ['test', 'test to value', ['phrase' => false] ]],
-            [['test:(test value)', 1], ['test', 'test and value', ['phrase' => false] ]],
-            [['test:(test value)', 1], ['test', 'test or value', ['phrase' => false] ]],
+            [['test:(test value)', 1], ['test', 'test not value', ['phrase' => false]]],
+            [['test:(test value)', 1], ['test', 'test to value', ['phrase' => false]]],
+            [['test:(test value)', 1], ['test', 'test and value', ['phrase' => false]]],
+            [['test:(test value)', 1], ['test', 'test or value', ['phrase' => false]]],
         ];
     }
 
@@ -202,9 +277,9 @@ class QueryRunnerTest extends TestCase
     {
         $data = [];
         $special_chars = ['\\', '+', '-', '&&', '||', '!', '(', ')', '{', '}', '[', ']', '^', '"', '~', '*', '?', ':'];
-        foreach($special_chars as $ch) {
+        foreach ($special_chars as $ch) {
             $expected = str_replace($ch, "\\{$ch}", "test {$ch} value");
-            $data[] = [["test:({$expected})", 1], ['test', "test {$ch} value", ['phrase' => false] ]];
+            $data[] = [["test:({$expected})", 1], ['test', "test {$ch} value", ['phrase' => false]]];
         }
         return $data;
     }
