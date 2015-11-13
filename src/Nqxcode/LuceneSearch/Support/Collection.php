6f088736e761a1383a24251497cf1928c3f972af
nqxcode <nc101ux@gmail.com>
2015-11-13 23:47:36 +0300

    Fixes for correct reloading of nonexistent models in result collection.

--- src/Nqxcode/LuceneSearch/Support/Collection.php
+++ src/Nqxcode/LuceneSearch/Support/Collection.php
@@ -4,41 +4,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Collection extends \Illuminate\Database\Eloquent\Collection
 {
-    protected $reloaded = false;
-
     /**
-     * Reload each item in collection.
+     * Reload each nonexistent item in collection.
      *
      * @return $this|static
      */
     public function reload()
     {
-        if (!$this->reloaded) {
-            $this->reloaded = true;
-
-            $items = array_map(
-                function (Model $sourceModel) {
-                    $primaryKey = $sourceModel->getKeyName();
-                    $primaryValue = $sourceModel->{$primaryKey};
-
-                    $query = $sourceModel->query();
-                    $query->where($primaryKey, $primaryValue);
-
-                    $targetModel = $query->first();
-
-                    return $targetModel;
-                },
-                $this->items
-            );
-
-            $items = array_filter(
-                $items,
-                function ($item) {
-                    return !is_null($item);
+        /** @var Model $source */
+        foreach($this->items as $i => $source) {
+            if (!$source->exists) {
+                /** @var Model $target */
+                $target = $source->find($source->{$source->getKeyName()});
+                if (!is_null($target)) {
+                    $source->setRawAttributes($target->getAttributes(), true);
+                    $source->exists = true;
+                } else {
+                    unset($this->items[$i]);
                 }
-            );
-
-            $this->items = $items;
+            }
         }
 
         return $this;
