b96331b79f4ed0b58e3ee65a76d011d0ed833072
nqxcode <nc101ux@gmail.com>
2017-04-08 23:59:55 +0300

    Reduce queries when get search result collection.
    (cherry picked from commit 7167675)
    
    (cherry picked from commit 73e5549)

---
+++ tests/models/Order.php
@@ -0,0 +1,14 @@
+<?php namespace tests\models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Order
+ *
+ * @property $name
+ * @package tests\models
+ */
+class Order extends Model
+{
+
+}
